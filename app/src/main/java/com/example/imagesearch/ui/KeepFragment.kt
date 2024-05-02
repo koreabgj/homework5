bf8789c19769198caeaa2cff02ec4aa61dcf449b
배기정 <ligerbae@gmail.com>
2024-05-02 17:53:58 +0900

    프래그먼트 리사이클러뷰

--- app/src/main/java/com/example/imagesearch/ui/KeepFragment.kt
+++ app/src/main/java/com/example/imagesearch/ui/KeepFragment.kt
@@ -1,60 +1,38 @@
 package com.example.imagesearch.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.imagesearch.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class KeepFragment : Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment2.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BlankFragment2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: KeepAdapter // 여기서 MyAdapter는 사용자가 직접 만든 어댑터 클래스입니다.
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank2, container, false)
+        val view = inflater.inflate(R.layout.fragment_keep, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = KeepAdapter()
+//        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // 여기서 adapter에 데이터를 설정하거나, 필요에 따라 데이터를 업데이트할 수 있습니다.
     }
 }
---
+++ app/src/main/java/com/example/imagesearch/ui/SearchAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.imagesearch.ui
+
+class SearchAdapter {
+
+}
